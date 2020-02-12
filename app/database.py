c8d80608273b8d2fd89833aea5207b14d0a6f925
Kacper Radzikowski <radzikowskikacper@gmail.com>
2020-02-13 02:01:49 +0900

    Changes in service

--- app/database.py
+++ app/database.py
@@ -4,13 +4,14 @@
 DB initialization
 """
 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 
 
-def init_db_schema(app):
+def init_db_schema(app: Flask):
     """Initializes database with models from the app
     """
     db.init_app(app)
--- app/main/service/tasks_service.py
+++ app/main/service/tasks_service.py
@@ -13,8 +13,8 @@ from app.main.model.models import Task
 def new_task(url: str, tasktype: str):
     """Creates a task
     """
-    task = Task(type=tasktype, url=url)
-    db.session.add(task)
+    task_ = Task(type=tasktype, url=url, status=True)
+    db.session.add(task_)
     db.session.commit()
 
 
@@ -22,11 +22,11 @@ def tasks() -> List[str]:
     """List of current tasks
     """
     tasks_list = Task.query.with_entities(Task.url).all()
-    return tasks_list
+    return [task_[0] for task_ in tasks_list]
 
 
 def task(url: str, tasktype: str) -> Dict[str, str]:
     """Task status
     """
-    task = Task.query.filter_by(url=url, type=tasktype)
-    return {'url': url, 'type': tasktype, 'status': task.status}
+    task_ = Task.query.filter_by(url=url, type=tasktype)
+    return {'url': url, 'type': tasktype, 'status': task_.status}
